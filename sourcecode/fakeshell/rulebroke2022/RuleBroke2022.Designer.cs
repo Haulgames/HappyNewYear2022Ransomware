cde963061c83cddc62d6ff9df4592aea3364a948
GlebYoutuber <73891760+GlebYoutuber@users.noreply.github.com>
2021-12-29 12:50:05 +0200

    Add files via upload

---
+++ sourcecode/fakeshell/rulebroke2022/RuleBroke2022.Designer.cs
@@ -0,0 +1,57 @@
+﻿
+namespace RuleBrokeHappyNewYear2022
+{
+    partial class RuleBroke2022
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RuleBroke2022));
+            this.SuspendLayout();
+            // 
+            // RuleBroke2022
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(900, 490);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "RuleBroke2022";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.RuleBroke2022_FormClosing);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ sourcecode/fakeshell/rulebroke2022/RuleBroke2022.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RuleBrokeHappyNewYear2022
+{
+    public partial class RuleBroke2022 : Form
+    {
+        public RuleBroke2022()
+        {
+            InitializeComponent();
+            Cursor.Hide();
+            this.TransparencyKey = Color.Transparent;
+            this.Text = "Shell is Overwrited";
+        }
+
+        private void RuleBroke2022_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = true;
+        }
+    }
+}
