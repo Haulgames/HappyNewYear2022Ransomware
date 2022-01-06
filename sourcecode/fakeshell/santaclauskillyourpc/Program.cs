9b851d8ed9dca327b5a6dc2724212b57c97b6601
GlebYoutuber <73891760+GlebYoutuber@users.noreply.github.com>
2022-01-06 19:32:23 +0200

    Update Program.cs

--- sourcecode/fakeshell/santaclauskillyourpc/Program.cs
+++ sourcecode/fakeshell/santaclauskillyourpc/Program.cs
@@ -8,9 +8,6 @@ namespace SantaClausWillKillYourPC
 {
     internal static class Program
     {
-        /// <summary>
-        /// Главная точка входа для приложения.
-        /// </summary>
         [STAThread]
         static void Main()
         {
