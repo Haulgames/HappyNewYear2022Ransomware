744637bccffd813c8f99d98e4f6b7ca0e393ec97
GlebYoutuber <73891760+GlebYoutuber@users.noreply.github.com>
2022-01-21 19:22:42 +0200

    Update Program.cs

--- sourcecode/fakeshell/rulebroke2022/Program.cs
+++ sourcecode/fakeshell/rulebroke2022/Program.cs
@@ -8,9 +8,6 @@ namespace RuleBrokeHappyNewYear2022
 {
     static class Program
     {
-        /// <summary>
-        /// Главная точка входа для приложения.
-        /// </summary>
         [STAThread]
         static void Main()
         {
